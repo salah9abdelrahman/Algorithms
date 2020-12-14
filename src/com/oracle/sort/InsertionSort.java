a809a5f3f57290fcebe07b3d1ccc7d7b4dcc8a01
salah <salah.abdelrahman@safe-sas.com>
2020-12-15 01:16:48 +0200

    recursive Insertion Sort

--- src/com/oracle/sort/InsertionSort.java
+++ src/com/oracle/sort/InsertionSort.java
@@ -7,7 +7,8 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {20, 35, -15, 7, 55, 1, -22};
 //        iterationInsertionSort(arr);
-        recursiveInsertionSort(arr, 1);
+//        recursiveInsertionSort(arr, 1);
+        recursiveInsertionSort2(arr, arr.length);
         UtilFunctions.print(arr);
 
 
@@ -15,23 +16,8 @@ public class InsertionSort {
 //        UtilFunctions.print(arr);
     }
 
-    static void recursiveInsertionSort(int[] input, int firstUnsortedIndex){
-        if(firstUnsortedIndex >= input.length){
-            return;
-        }
-        int newElement = input[firstUnsortedIndex];
 
-        int i;
-        for (i = firstUnsortedIndex; i > 0 && input[i - 1] > newElement; i--) {
-            input[i] = input[i - 1];
-        }
-
-        input[i] = newElement;
-        firstUnsortedIndex++;
-        recursiveInsertionSort(input, firstUnsortedIndex);
-    }
-
-    static void iterationInsertionSort(int[] arr){
+    static void iterationInsertionSort(int[] arr) {
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
             int newElement = arr[firstUnsortedIndex];
             /*
@@ -48,4 +34,47 @@ public class InsertionSort {
             arr[i] = newElement;
         }
     }
+
+    static void recursiveInsertionSort(int[] input, int firstUnsortedIndex) {
+        if (firstUnsortedIndex >= input.length) {
+            return;
+        }
+
+
+        int newElement = input[firstUnsortedIndex];
+
+        int i;
+        for (i = firstUnsortedIndex; i > 0 && input[i - 1] > newElement; i--) {
+            input[i] = input[i - 1];
+        }
+
+        input[i] = newElement;
+        ++firstUnsortedIndex;
+        recursiveInsertionSort(input, firstUnsortedIndex);
+    }
+
+
+    static void recursiveInsertionSort2(int[] input, int numOfItems) {
+        if (numOfItems == 1) {
+            return;
+        }
+        recursiveInsertionSort2(input, numOfItems - 1);
+
+        int newElement = input[numOfItems -1];
+            /*
+            i > 0: we haven't hit the first of the array
+            arr[i-1] > newElement: keep traversing as long as the element we are looking at in
+            traversing is greater than the new element we want to insert
+
+             */
+        int i;
+        for (i = numOfItems -1; i > 0 && input[i - 1] > newElement; i--) {
+            input[i] = input[i - 1];
+        }
+
+        input[i] = newElement;
+
+
+    }
+
 }

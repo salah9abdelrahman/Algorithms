49a809ade3539454cbc47823f1a355b4ae9384bb
salah <salah.abdelrahman@safe-sas.com>
2020-12-15 00:25:56 +0200

    Radix sort

---
+++ src/com/oracle/sort/RadixSort.java
@@ -0,0 +1,52 @@
+package com.oracle.sort;
+
+import com.oracle.UtilFunctions;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class RadixSort {
+    public static void main(String[] args) {
+
+        int[] arr = {4725, 4586, 1330, 8792, 1594, 5729};
+        radixSort(arr, 10, 4);
+
+        UtilFunctions.print(arr);
+    }
+
+    public static void radixSort(int[] input, int radix, int width) {
+        for (int position = 0; position < width; position++) {
+            radixSingleSort(input, position, radix);
+        }
+    }
+
+    private static void radixSingleSort(int[] input, int position, int radix) {
+        int[] countArray = new int[radix];
+
+        //normal count array
+        for (int i = 0; i < input.length; i++) {
+            countArray[getDigit(position, input[i], radix)]++;
+        }
+
+        /*
+        adjust the count array to achieve stability  by count how many elements has a value or less
+        e.g: how many elements have the value 3 or less than 3.
+         */
+        for (int i = 1; i < countArray.length; i++) {
+            countArray[i] += countArray[i - 1];
+        }
+
+        int[] temp = new int[input.length];
+
+        for (int tempIndex = input.length - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, input[tempIndex], radix)]] = input[tempIndex];
+        }
+
+        System.arraycopy(temp, 0, input, 0, input.length);
+
+    }
+
+    private static int getDigit(int position, int itemValue, int radix) {
+        return (itemValue / (int) Math.pow(radix, position)) % radix;
+    }
+}
